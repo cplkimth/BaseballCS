59ee812a9388db9d7bf5083432eb399913beca69
cplkimth <cplkimth@naver.com>
2020-08-19 10:23:57 +0900

    .

--- StarCraft/Marine.cs
+++ StarCraft/Marine.cs
@@ -8,15 +8,50 @@ namespace StarCraft
 {
     class Marine
     {
-        public int X;
-        public int Y;
-        private int hp;
+        public static int MaxHP = 50; // static member
+
+        public void Heal()
+        {
+            _hp = MaxHP;
+        }
+
+        // auto property
+        public int X { get; set; }
+
+        public int Y { get; private set; }
+
+        private int _hp; // instance member
 
         public Marine()
         {
             X = 1;
             Y = 1;
-            hp = 50;
+            _hp = 50;
+        }
+
+        // getter
+        public int GetHP()
+        {
+            return _hp;
+        }
+
+        // setter
+        public void SetHP(int hp)
+        {
+            _hp = hp;
+        }
+
+        // sugar syntax
+        public int HP
+        {
+            get
+            {
+                return _hp;
+            }
+            set
+            {
+                _hp = value;
+            }
         }
 
         public void Move(int x, int y)
@@ -27,9 +62,9 @@ namespace StarCraft
 
         public void BeAttacked(int damage)
         {
-            while (damage > 0 && hp > 0)
+            while (damage > 0 && _hp > 0)
             {
-                hp--;
+                _hp--;
                 damage--;
             }
         }
--- StarCraft/Program.cs
+++ StarCraft/Program.cs
@@ -11,12 +11,29 @@ namespace StarCraft
         static void Main(string[] args)
         {
             Marine m1 = new Marine();
+            m1.X = 1;
+            //m1.Y = 2;
+
+            Marine m2 = new Marine();
+
+            m1.BeAttacked(5);
+            m2.BeAttacked(10);
+            Console.WriteLine(m1.HP);
+            Console.WriteLine(m2.HP);
+
+            Console.WriteLine(Marine.MaxHP);
+            Console.WriteLine(Marine.MaxHP);
+
             
             m1.BeAttacked(100);
 
-            m1.hp = 30;
-            Console.WriteLine(m1.hp);
+            m1.SetHP(30);
+            Console.WriteLine(m1.GetHP());
+            m1.SetHP(m1.GetHP() + 1);
 
+            m1.HP = 30;
+            Console.WriteLine(m1.HP);
+            m1.HP = m1.HP + 1;
         }
     }
 }
