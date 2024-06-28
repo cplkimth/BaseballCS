caeecb18f108bc4cefd2dac330ebe5d492e12dd1
김태현 <cplkimth@naver.com>
2024-06-28 16:13:37 +0900

    04 collection

--- BaseballCs/Program.cs
+++ BaseballCs/Program.cs
@@ -1,6 +1,8 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 #endregion
 
@@ -15,24 +17,21 @@ internal class Program
     {
         // 1. 정답을 생성한다.
         var random = new Random(1);
-        int answer0;
-        int answer1;
-        int answer2;
-
+        List<int> answers = new List<int>();
+        
         while (true)
         {
-            answer0 = random.Next(MaxValue);
-            answer1 = random.Next(MaxValue);
-            answer2 = random.Next(MaxValue);
-
-            if (answer0 != answer1 && answer1 != answer2 && answer2 != answer0)
+            answers.Clear();
+            for (int i = 0; i < Digit; i++)
+                answers.Add(random.Next(MaxValue));
+            
+            if (answers.Distinct().Count() == Digit)
                 break;
         }
 
         Console.WriteLine("[정답]");
-        Console.Write(answer0 + " ");
-        Console.Write(answer1 + " ");
-        Console.Write(answer2 + " ");
+        foreach (int answer in answers)
+            Console.Write($"{answer} ");
         Console.WriteLine();
 
 
@@ -41,14 +40,16 @@ internal class Program
         while (true)
         {
             // 2. 추측을 입력받는다.
-            int guess0 = int.Parse(Console.ReadLine());
-            int guess1 = int.Parse(Console.ReadLine());
-            var guess2 = int.Parse(Console.ReadLine());
-
+            var guesses = new List<int>();
+            for (int i = 0; i < Digit; i++)
+            {
+                int guess = int.Parse(Console.ReadLine());
+                guesses.Add(guess);
+            }
+            
             Console.WriteLine("[추측]");
-            Console.Write(guess0 + " ");
-            Console.Write(guess1 + " ");
-            Console.Write(guess2 + " ");
+            foreach (var guess in guesses)
+                Console.Write($"{guess} ");
             Console.WriteLine();
 
 
@@ -59,27 +60,18 @@ internal class Program
             var ball = 0;
             var @out = 0;
 
-            if (guess0 == answer0)
-                strike++;
-            else if (guess0 == answer1 || guess0 == answer2)
-                ball++;
-            else
-                @out++;
+            for (int i = 0; i < Digit; i++)
+            {
+                int j = (i + 1) % Digit;
+                int k = (i + 2) % Digit;
 
-            if (guess1 == answer1)
-                strike++;
-            else if (guess1 == answer2 || guess1 == answer0)
-                ball++;
-            else
-                @out++;
-
-            if (guess2 == answer2)
-                strike++;
-            else if (guess2 == answer0 || guess2 == answer1)
-                ball++;
-            else
-                @out++;
-
+                if (guesses[i] == answers[i])
+                    strike++;
+                else if (guesses[i] == answers[j] || guesses[i] == answers[k])
+                    ball++;
+                else
+                    @out++;
+            }
             Console.WriteLine($"[S]{strike} [B]{ball} [O]{@out}");
 
 
